8e2528821614f8e38388821e39d140fab82ad669
kozhamseitova <kozhamseitova91@gmail.com>
2022-04-26 13:59:57 +0600

    university controller

---
+++ diploma/database/migrations/2022_04_25_140304_create_universities_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use \App\Models\Region;
+use \App\Models\Category;
+use \App\Models\Type;
+use \App\Models\Language;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('universities', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->string('website')->nullable();
+            $table->string('phone_number');
+            $table->string('instagram')->nullable();
+            $table->foreignIdFor(Region::class)->constrained();
+            $table->foreignIdFor(Category::class)->constrained();
+            $table->foreignIdFor(Type::class)->constrained();
+            $table->foreignIdFor(Language::class)->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('universities');
+    }
+};
